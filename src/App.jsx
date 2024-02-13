c9b426a34a57044ec6ff017014f8e2084ef0eb3c
twój login do GitHuba <twj email>
2024-02-13 13:21:56 +0100

    configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stay Healthy</h1>
+      <p>My first React app</p>
+    </div>
+  );
+}
+
+export default App;
