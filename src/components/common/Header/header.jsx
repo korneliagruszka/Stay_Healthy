65a7c514dc41d02155d7c01a3618d2fe1097155c
twój login do GitHuba <twj email>
2024-02-13 15:13:33 +0100

    main_page_header_tbc

--- src/components/common/Header/header.jsx
+++ src/components/common/Header/header.jsx
@@ -1,1 +1,20 @@
 import '../Header/header.scss';
+
+function Header() {
+    return (
+      <div className="Header">
+        <h1>
+        <span className="header_first">Stay</span>
+        <span className="header_second">Healthy</span>
+        </h1>
+        <nav className="navigation">
+          <a href="">Zaplanuj trening</a>
+          <a href="">Kalkulator BMI</a>
+          <a href="">Śledź postępy</a>
+          <a href="">Kontakt</a>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Header;
