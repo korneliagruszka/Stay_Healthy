467303bef92fa9470769dec3c037adb499d97bec
twój login do GitHuba <twj email>
2024-02-15 13:33:09 +0100

    main_page_banner_done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Banner from "./components/common/Banner/banner";
 import Header from "./components/common/Header/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
